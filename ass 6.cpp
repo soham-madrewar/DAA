b60353cc33bb31926d1cc33cdaa23c7f1b6456d2
soham-madrewar <160596157+soham-madrewar@users.noreply.github.com>
2025-11-05 10:44:30 +0530

    Refactor knapsack problem implementation

--- ass 6.cpp
+++ ass 6.cpp
@@ -53,23 +53,23 @@ int main() {
         cin >> items[i].utility;
     }
 
-    // dp[i][w] = max utility using first i items with capacity w
+
     vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
-    // keep[i][w] = true if we take item (i-1) in optimal for dp[i][w]
+   
     vector<vector<char>> keep(n + 1, vector<char>(capacity + 1, 0));
 
     for (int i = 1; i <= n; ++i) {
         int wt = items[i - 1].weight;
         int val = items[i - 1].utility;
         for (int w = 0; w <= capacity; ++w) {
-            // Option 1: don't take item i
+           
             dp[i][w] = dp[i - 1][w];
-            // Option 2: take item i (if fits)
+           
             if (wt <= w) {
                 int takeVal = val + dp[i - 1][w - wt];
                 if (takeVal > dp[i][w]) {
                     dp[i][w] = takeVal;
-                    keep[i][w] = 1; // mark that we took item i-1
+                    keep[i][w] = 1; 
                 }
             }
         }
@@ -78,12 +78,12 @@ int main() {
     int maxUtility = dp[n][capacity];
     cout << "\nMax utility = " << maxUtility << "\n";
 
-    // Reconstruct chosen items
+   
     vector<int> chosen;
     int w = capacity;
     for (int i = n; i >= 1; --i) {
         if (keep[i][w]) {
-            chosen.push_back(i - 1); // store index of chosen item
+            chosen.push_back(i - 1); 
             w -= items[i - 1].weight;
         }
     }
@@ -92,7 +92,7 @@ int main() {
         cout << "No items chosen.\n";
     } else {
         cout << "Items chosen (index, weight, utility):\n";
-        // print in original input order
+        
         for (int i = chosen.size() - 1; i >= 0; --i) {
             int idx = chosen[i];
             cout << "Item " << (idx + 1)
@@ -103,4 +103,5 @@ int main() {
 
     return 0;
 }
+
 
